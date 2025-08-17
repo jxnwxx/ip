3ff316e41ae4d19fc8cf6f2980f7b0fb848dc297
jxnwxx <49605139+jxnwxx@users.noreply.github.com>
2025-08-17 13:02:41 +0800

    Add user input storage and list display functionality

--- src/main/java/Peppy.java
+++ src/main/java/Peppy.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Peppy {
     public static void print(String string) {
@@ -8,7 +9,21 @@ public class Peppy {
         System.out.println("\t" + horizontalLine);
     }
 
+    public static void printList(ArrayList<String> list) {
+        if (list.isEmpty()) {
+            print("there's nothing in the list!");
+        } else {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < list.size(); i++) {
+                result.append(String.format("%d. %s \n\t ", i + 1, list.get(i)));
+            }
+            print(result.toString().stripTrailing());
+        }
+    }
+
     public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+
         String welcomeString = "Hello! I'm Peppy\n\t What can I do for you?";
         print(welcomeString);
 
@@ -16,9 +31,14 @@ public class Peppy {
 
         while (true) {
             String input = scanner.nextLine();
-            if (input.equals("bye"))
+            if (input.equals("bye")) {
                 break;
-            print(input);
+            } else if (input.equals("list")) {
+                printList(list);
+            } else {
+                list.add(input);
+                print("added: " + input);
+            }
         }
 
         print("Bye. Hope to see you again soon!");
