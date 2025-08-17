9ba3a0859854e03ac30b7a93ad8585522e419e2d
jxnwxx <49605139+jxnwxx@users.noreply.github.com>
2025-08-17 15:37:26 +0800

    Implement marking and unmarking of tasks

--- src/main/java/Peppy.java
+++ src/main/java/Peppy.java
@@ -9,20 +9,44 @@ public class Peppy {
         System.out.println("\t" + horizontalLine);
     }
 
-    public static void printList(ArrayList<String> list) {
+    public static void printList(ArrayList<Task> list) {
         if (list.isEmpty()) {
             print("there's nothing in the list!");
         } else {
             StringBuilder result = new StringBuilder();
             for (int i = 0; i < list.size(); i++) {
-                result.append(String.format("%d. %s \n\t ", i + 1, list.get(i)));
+                result.append(String.format("%d. %s \n\t ",
+                        i + 1,
+                        list.get(i).toString()));
             }
             print(result.toString().stripTrailing());
         }
     }
 
+    public static void markDone(ArrayList<Task> list, Integer index) {
+        if (index > list.size()) {
+            print("index provided is out of range!");
+        } else {
+            Task task = list.get(index - 1);
+            task.markDone();
+            print(String.format("Nice! I've marked this task as done:\n\t  %s",
+                    task.toString()));
+        }
+    }
+
+    public static void markUndone(ArrayList<Task> list, Integer index) {
+        if (index > list.size()) {
+            print("index provided is out of range!");
+        } else {
+            Task task = list.get(index - 1);
+            task.markUndone();
+            print(String.format("Nice! I've marked this task as not done yet:\n\t  %s",
+                    task.toString()));
+        }
+    }
+
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<Task> list = new ArrayList<Task>();
 
         String welcomeString = "Hello! I'm Peppy\n\t What can I do for you?";
         print(welcomeString);
@@ -35,8 +59,15 @@ public class Peppy {
                 break;
             } else if (input.equals("list")) {
                 printList(list);
+            } else if (input.startsWith("mark")) {
+                Integer index = Integer.parseInt(input.substring(5));
+                markDone(list, index);
+            } else if (input.startsWith("unmark")) {
+                Integer index = Integer.parseInt(input.substring(7));
+                markUndone(list, index);
             } else {
-                list.add(input);
+                Task task = new Task(input);
+                list.add(task);
                 print("added: " + input);
             }
         }
