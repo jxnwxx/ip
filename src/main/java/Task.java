3309e9c9a554fda886f38fdd0f5b0b97c45924ce
jxnwxx <49605139+jxnwxx@users.noreply.github.com>
2025-08-17 15:33:41 +0800

    Implement Task.java class to represent tasks

---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markUndone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%c] %s", this.isDone ? 'X' : ' ',this.description);
+    }
+}
