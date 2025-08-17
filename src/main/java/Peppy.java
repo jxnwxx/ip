5ba52bec2eb622019ae2478c4e4843166ef8cdf5
jxnwxx <49605139+jxnwxx@users.noreply.github.com>
2025-08-17 12:33:05 +0800

    Added echo functionality from user input

--- src/main/java/Peppy.java
+++ src/main/java/Peppy.java
@@ -1,11 +1,26 @@
-public class Peppy {
-    public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Peppy");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
+import java.util.Scanner;
 
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+public class Peppy {
+    public static void print(String string) {
+        String horizontalLine = "____________________________________________________________";
+        System.out.println("\t" + horizontalLine);
+        System.out.println("\t " + string);
+        System.out.println("\t" + horizontalLine);
+    }
+
+    public static void main(String[] args) {
+        String welcomeString = "Hello! I'm Peppy\n\t What can I do for you?";
+        print(welcomeString);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye"))
+                break;
+            print(input);
+        }
+
+        print("Bye. Hope to see you again soon!");
     }
 }
